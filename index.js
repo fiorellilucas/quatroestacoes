aae7db9ffdb2b376a26523b2ba0d11bbfa1c62ff
Lucas Fiorelli <fiorellilucas@gmail.com>
2022-10-05 01:57:54 -0300

    Adicionar rotas para buscar e adicionar reservas

--- index.js
+++ index.js
@@ -11,23 +11,44 @@ app.get("/api", (req, res) => {
   res.json("hello world")
 })
 
-app.get("/api/moradores", async (req, res) => {
-  const moradores = await prisma.morador.findMany()
+app
+  .route("/api/moradores")
+  .get(async (req, res) => {
+    const moradores = await prisma.morador.findMany()
 
-  // é necessário converter o campo 'celular' de BigInt do banco de dados para String, já que o serializer do JS não suporta BigInt.
-  moradores.forEach((morador) => {
-    morador["celular"] = String(morador["celular"])
+    // é necessário converter o campo 'celular' de BigInt do banco de dados para String, já que o serializer do JS não suporta BigInt.
+    moradores.forEach((morador) => {
+      morador["celular"] = String(morador["celular"])
+    })
+
+    res.json(moradores)
   })
 
-  res.json(moradores)
-})
-
-app.post("/api/moradores", async (req, res) => {
-  await prisma.morador.create({
-    data: req.body
+  .post(async (req, res) => {
+    await prisma.morador.create({
+      data: req.body,
+    })
+    res.end()
   })
-  res.end()
-})
+
+app
+  .route("/api/reservas")
+  .get(async (req, res) => {
+    const reservas = await prisma.reserva.findMany()
+    res.json(reservas)
+  })
+
+  .post(async (req, res) => {
+    let reserva = req.body
+    const data = new Date(reserva["data"])
+    reserva["data"] = data
+
+    await prisma.reserva.create({
+      data: reserva,
+    })
+
+    res.end()
+  })
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`)
