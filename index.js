dd62bcaca3cc67116f470187898178d7fa42a0cf
Lucas Fiorelli <fiorellilucas@gmail.com>
2022-10-05 01:02:03 -0300

    Adicionar rotas para adicionar e buscar moradores

--- index.js
+++ index.js
@@ -2,8 +2,31 @@ const express = require("express")
 const app = express()
 const PORT = 3000
 
-app.get("/", (req, res) => {
-  res.send("hello world")
+const { PrismaClient } = require("@prisma/client")
+const prisma = new PrismaClient()
+
+app.use(express.json())
+
+app.get("/api", (req, res) => {
+  res.json("hello world")
+})
+
+app.get("/api/moradores", async (req, res) => {
+  const moradores = await prisma.morador.findMany()
+
+  // é necessário converter o campo 'celular' de BigInt do banco de dados para String, já que o serializer do JS não suporta BigInt.
+  moradores.forEach((morador) => {
+    morador["celular"] = String(morador["celular"])
+  })
+
+  res.json(moradores)
+})
+
+app.post("/api/moradores", async (req, res) => {
+  await prisma.morador.create({
+    data: req.body
+  })
+  res.end()
 })
 
 app.listen(PORT, () => {
