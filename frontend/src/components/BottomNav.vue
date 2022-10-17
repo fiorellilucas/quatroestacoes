3cf3482f6ffccdf388b53b1b855e09e996f7c217
Lucas Fiorelli <fiorellilucas@gmail.com>
2022-10-16 22:46:06 -0300

    Deletar arquivos iniciais e criar Bottom Navigation simples

---
+++ frontend/src/components/BottomNav.vue
@@ -0,0 +1,34 @@
+<script setup></script>
+
+<template>
+  <nav>
+    <a href="#">Home</a>
+    <a href="#">Moradores</a>
+    <a href="#">Calendário</a>
+    <a href="#">Outros</a>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  height: 8%;
+  width: 100%;
+}
+
+a {
+  flex: 1 1 0;
+  text-align: center;
+  text-decoration: none;
+  color: black
+}
+
+a:visited {
+  text-decoration: none;
+}
+
+</style>
